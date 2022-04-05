ae69057e63665c79739fa4a200307bb17289ae26
Diyor Mirpolatov <diyormirpolatov01@gmail.com>
2022-04-05 17:24:58 +0500

    Diyor branchda register boldi

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,31 +1,87 @@
 import React from "react";
 import "./Register.scss";
-import { Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate()
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+
+    axios
+      .post("https://telegram-alisherjon-api.herokuapp.com/users", data)
+      .then((res) => {
+        console.log(res.data);
+        const { token } = res.data;
+        const { status } = res.data;
+        localStorage.setItem("TOKEN", token);
+        if(status === 201) {
+          navigate("/user")
+        }
+      })
+      .catch((err) => {
+        const { message } = err.response.data;
+        console.log(message);
+        if(message === "User Already Exists") {
+          alert("User Already Exists! Please choose another one!!!😁 ");
+          e.target.reset()
+        }
+      });
+  };
 
   return (
     <>
       <div className="register">
-        <form>
+        <form onSubmit={handleSubmit}>
           <Link to={"/login"}>I have an account</Link>
 
-          <input type="text" id="name" required autoComplete="off"/>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            required
+            autoComplete="off"
+          />
           <label htmlFor="name" className="labName">
             Name
           </label>
 
-          <input type="number" id="phNumber" required autoComplete="off"/>
-          <label htmlFor="phNumber" className="labPh">Phone Number</label>
+          <input
+            type="number"
+            id="phNumber"
+            name="phone"
+            required
+            autoComplete="off"
+          />
+          <label htmlFor="phNumber" className="labPh">
+            Phone Number
+          </label>
 
-          <input type="text" id="username" required autoComplete="off"/>
-          <label htmlFor="username" className="labUs">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            required
+            autoComplete="off"
+          />
+          <label htmlFor="username" className="labUs">
+            Username
+          </label>
 
-          <input type="text" id="password" required autoComplete="off"/>
-          <label htmlFor="password" className="labPs">Password</label>
-
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            autoComplete="off"
+          />
+          <label htmlFor="password" className="labPs">
+            Password
+          </label>
 
           <button type="submit">Register</button>
         </form>

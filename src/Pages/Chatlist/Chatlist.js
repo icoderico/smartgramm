4676ca577067329439efbe9301771f024f7902b3
SHUHRAT ERGASHEV <ergashevshuhrat744@gmail.com>
2022-04-06 07:16:06 +0500

    stilla tugadi

--- src/Pages/Chatlist/Chatlist.js
+++ src/Pages/Chatlist/Chatlist.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "./Chatlist.scss";
+import { Link, useNavigate } from "react-router-dom";
 
 const Chatlist = () => {
-  return (
-    <div>Chatlist</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Chatlist
+  navigate('/lichka')
+
+  return (
+    <>
+      <div className="chatlistWrap">
+        <ul>
+          <li onClick={navigate}>
+            <div className="circleIcon"></div>
+            <div className="friendsName">Diyor Mirpolatov</div>
+          </li>
+          <li>
+            <div className="circleIcon"></div>
+            <div className="friendsName">Dilshod Hojakov</div>
+          </li>
+          <li>
+            <div className="circleIcon"></div>
+            <div className="friendsName">Shaxnoza</div>
+          </li>
+          <li>
+            <div className="circleIcon"></div>
+            <div className="friendsName">Shaxnoza</div>
+          </li>
+          <li>
+            <div className="circleIcon"></div>
+            <div className="friendsName">Shaxnoza</div>
+          </li>
+          <li>
+            <div className="circleIcon"></div>
+            <div className="friendsName">Shaxnoza</div>
+          </li>
+          <li>
+            <div className="circleIcon"></div>
+            <div className="friendsName">Shaxnoza</div>
+          </li>
+          <li>
+            <div className="circleIcon"></div>
+            <div className="friendsName">Shaxnoza</div>
+          </li>
+          <li>
+            <div className="circleIcon"></div>
+            <div className="friendsName">Shaxnoza</div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Chatlist;
